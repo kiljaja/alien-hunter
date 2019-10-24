5a91b8a1c73b4c5d6df788dd6b428e61bdb0d45a
ejfranco06 <ejfranco06@gmail.com>
2019-10-24 11:57:23 -0400

    Add Player Shot

---
+++ Assets/Scripts/PlayerShotController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShotController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb2d;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        SetDirection(Vector2.up);
+    }
+
+    void Update()
+    {
+
+    }
+
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.layer != 13)
+        {
+            SelfDestroy();
+        }
+
+    }
+    void SelfDestroy()
+    {
+        Destroy(gameObject);
+    }
+
+    public void SetDirection(Vector2 direction)
+    {
+        rb2d.velocity = direction.normalized * speed;
+    }
+}
