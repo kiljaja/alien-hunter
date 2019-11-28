0e13eee09659aa1cea00fbb7c258d2f1a766d9f3
ejfranco06 <ejfranco06@gmail.com>
2019-11-27 19:12:38 -0500

    Add health bar

---
+++ Assets/Scripts/Health/HealthBarController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarController : MonoBehaviour
+{
+    private Transform bar;
+    private void Start()
+    {
+        if(bar == null) bar = transform.Find("Bar");
+    }
+
+    public void SetHealthPercent(float newPercent){
+        //Prevent wrong input
+        if(newPercent < 0f || newPercent > 1f) return;
+        bar.localScale = new Vector3(newPercent, 1f, 1f);
+    }
+
+    public void SetHealthColor(Color color){
+        bar.Find("HealthColor").GetComponent<SpriteRenderer>().color = color;
+    }
+
+    
+}
