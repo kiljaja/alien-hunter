8bc76cf0e9025e307476d154192999db83dba42c
ejfranco06 <ejfranco06@gmail.com>
2019-11-28 09:19:08 -0500

    Add healthbar to player

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,16 +6,26 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] 
     private float speed = 2;
+    private int health = 100;
+    private int MAX_HEALTH = 100;
+    private Color HEALTH_BAR_COLOR = new Color(0, 255, 0, 255);
     public GameObject projectile;
     private Rigidbody2D rb2d;
     private Animator anim;
+    private SpriteRenderer spriteRenderer;
+    private HealthBarController healthBar;
     private float verticalMovement;
     private float horizontalMovement;
     
     void Start()
     {
+        health = GetHealth();
         if(anim == null) anim = GetComponent<Animator>();
         if(rb2d == null ) rb2d = GetComponent<Rigidbody2D>();
+        if(healthBar == null) healthBar = GetComponentInChildren<HealthBarController>();
+        if(spriteRenderer == null) spriteRenderer = GetComponent<SpriteRenderer>();
+        healthBar.SetHealthColor(HEALTH_BAR_COLOR);
+        UpdateHealthBar();
     }
 
     void Update()
@@ -28,7 +38,7 @@ public class PlayerController : MonoBehaviour
 
         //For testing 
         if(Input.GetKeyDown(KeyCode.P)){
-            Die();
+            TakeDamage(Random.Range(20, 30));
         }
         
     }
@@ -45,7 +55,7 @@ public class PlayerController : MonoBehaviour
     }
 
     void Shoot(){
-        float yOffset = 1.4f;
+        float yOffset = (spriteRenderer.bounds.size.y/ 2) + .5f;
         Vector2 spawnLocation = new Vector2(this.transform.position.x, this.transform.position.y + yOffset);
         GameObject newBullet = Instantiate(projectile, spawnLocation, Quaternion.identity) as GameObject;
     }
@@ -53,6 +63,25 @@ public class PlayerController : MonoBehaviour
     private void Die(){
         anim.SetTrigger("Die");
         Destroy(gameObject, .750f);
+    }
+
+    private int GetHealth(){
+        int savedHealth = 100;
+        return (savedHealth < health)? savedHealth : health;
+    }
+
+    private void TakeDamage(int damage){
+        health -= damage;
+        if(health <= 0){
+            health = 0;
+            Die();
+        }
+        UpdateHealthBar();
+    }
+
+    private void UpdateHealthBar(){
+        float healthPercent = health / (float)MAX_HEALTH;
+        healthBar.SetHealthPercent(healthPercent);
     }
 
 }
