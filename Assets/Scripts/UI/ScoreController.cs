2e1a7f943668e9fa6cf4c4c067c820d8979e05eb
ejfranco06 <ejfranco06@gmail.com>
2019-11-30 18:23:21 -0500

    Add player score

---
+++ Assets/Scripts/UI/ScoreController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreController : MonoBehaviour
+{
+    private TextMeshPro textContainer;
+    void Start()
+    {
+        if (textContainer == null) textContainer = GetComponentInChildren<TextMeshPro>();
+        UpdateScore(GetScore());
+
+    }
+
+    public void UpdateScore(int score){
+        if(score < 0) score = 0;
+        string formatedScore = score.ToString().PadLeft(3, '0');
+        textContainer.text = formatedScore;
+    }
+
+    public int GetScore(){
+        return 200;
+    }
+}
